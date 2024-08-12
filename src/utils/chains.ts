d9199c9d43d7d920df31164e38f86a774da66902
boxfox619 <boxfoxsg619@gmail.com>
2024-08-12 23:46:53 +0900

    Redeploy subgraph

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -78,7 +78,7 @@ export function getSubgraphConfig(): SubgraphConfig {
   // subgraph does not support case switch with strings, hence this if else block
   if (selectedNetwork == MAINNET_NETWORK_NAME) {
     return {
-      factoryAddress: '0x2803a1fDA2C6DcDaD0e046459bc5728697deb777',
+      factoryAddress: '0x16C7B570267eb3e1F9576fed90721d5Ee540DB53',
       stablecoinWrappedNativePoolAddress: '',
       stablecoinIsToken0: false,
       wrappedNativeAddress: '0x7507c1dc16935B82698e4C63f2746A2fCf994dF8',
